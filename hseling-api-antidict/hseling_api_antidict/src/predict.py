b6449f0b0ac8b7e1bde33979c9a0f7049a5924a2
lapythie <55333322+lapythie@users.noreply.github.com>
2021-03-26 23:05:34 +0300

    Update predict.py
    Add pymorphy2 filter.
    Update 2 classifiers.
    Add hardcoded thresholds.

--- hseling-api-antidict/hseling_api_antidict/src/predict.py
+++ hseling-api-antidict/hseling_api_antidict/src/predict.py
@@ -4,16 +4,19 @@ from razdel import tokenize
 import regex
 import stopwordsiso
 from typing import List, Union, Dict, Any, Set
+from pymorphy2 import MorphAnalyzer
 
 LOCAL_FT_PATH = "hseling-data-antidict/api/models/fasttext/araneum_none_fasttextcbow_300_5_2018.model"
-LOCAL_LOANWORD_CLF_PATH = "hseling-api-antidict/hseling_api_antidict/models/classifier.pkl"
+LOCAL_LOANWORD_CLF_PATH = "hseling-api-antidict/hseling_api_antidict/models/lw_cb_classifier.pkl"
 LOCAL_OBSCENE_CLF_PATH = "hseling-api-antidict//hseling_api_antidict/models/cb_classifier.pkl"
-LOCAL_EXPRESSIVE_CLF_PATH = "hseling-api-antidict//hseling_api_antidict/models/expressive_classifier.pkl"
+LOCAL_EXPRESSIVE_CLF_PATH = "hseling-api-antidict//hseling_api_antidict/models/affixed_cb_classifier.pkl"
 
 DOCKER_FT_PATH = "/data/models/fasttext/araneum_none_fasttextcbow_300_5_2018.model"
-DOCKER_LOANWORD_CLF_PATH = "/app/hseling_api_antidict/models/classifier.pkl"
+DOCKER_LOANWORD_CLF_PATH = "/app/hseling_api_antidict/models/lw_cb_classifier.pkl"
 DOCKER_OBSCENE_CLF_PATH = "/app/hseling_api_antidict/models/cb_classifier.pkl"
-DOCKER_EXPRESSIVE_CLF_PATH = "/app/hseling_api_antidict/models/expressive_classifier.pkl"
+DOCKER_EXPRESSIVE_CLF_PATH = "/app/hseling_api_antidict/models/affixed_cb_classifier.pkl"
+
+morph = MorphAnalyzer()
 
 stops = set("""чей свой из-за вполне вообще вроде сюда аж той
 россия россии россию россией путин путина путину путиным путине
@@ -61,14 +64,16 @@ def is_word(token: str, min_len: int, max_len: int, s_words: Set[str]) -> bool:
 
 def predict(text: str) -> List[Dict[str, Union[List[Dict[str, Any]], dict]]]:
     tokens = [t.text for t in tokenize(text)]
-    cache = {t: {"loanword": 0, "obscene": 0, "expressive": 0}
+    cache = {t: {"loanword": 0, "obscene": 0, "expressive": 0, "tag": morph.parse(t)[0].tag}
              for t in set(t.lower() for t in tokens)}
     for t in cache:
         if is_word(t, min_len=3, max_len=30, s_words=stops):
             cache[t]["emb"] = model[t]
-            cache[t]["loanword"] = loanword_clf.predict([cache[t]["emb"]]).item()
+            cache[t]["loanword"] = (0 if (len(t) < 4) or ("Name" in cache[t]["tag"]) or ("Surn" in cache[t]["tag"])
+                                    else (loanword_clf.predict_proba([cache[t]["emb"]])[:, 1] > 0.72).item())
             cache[t]["obscene"] = obscene_clf.predict([cache[t]["emb"]]).item()
-            cache[t]["expressive"] = expressive_clf.predict([cache[t]["emb"]]).item()
+            cache[t]["expressive"] = (0 if (len(t) < 6) or ("Surn" in cache[t]["tag"])
+                                      else (expressive_clf.predict_proba([cache[t]["emb"]])[:, 1] > 0.72).item())
     analysis = [{"word": t,
                  "loanword": cache[t.lower()]["loanword"],
                  "obscene": cache[t.lower()]["obscene"],
